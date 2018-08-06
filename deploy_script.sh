4460287d894f1793d1dfbdf26c67d8cdd83c09bd
Sarbashis Das <sarbashis@users.noreply.github.com>
2018-08-06 12:57:48 +0200

    Publication Updated

---
+++ deploy_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf _site
+
+# if you're using Github Pages as andimiya.github.io, replace 'gh-pages' below with 'master'
+git clone -b master `git config remote.origin.url` _site
+bundle exec jekyll serve
+cd _site
+cp ../index_year.html publications/
+git add .
+#git commit -m "Fixed the page order"
+git commit -m "Publication Updated"
+git push
