3ac7d143e90eb433069816cae887c60960fcd9d9
Sarbashis Das <sarbashis@users.noreply.github.com>
2018-10-02 09:57:10 +0200

    read

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf _site
-
+message=read
 # if you're using Github Pages as andimiya.github.io, replace 'gh-pages' below with 'master'
 git clone -b master `git config remote.origin.url` _site
 bundle exec jekyll serve
@@ -13,5 +13,5 @@ git rm minimal-mistakes-jekyll.gemspec
 echo minimal-mistakes-jekyll.gemspec >> .gitignore
 echo deploy_script.sh >> .gitignore
 #git commit -m "Fixed the page order"
-git commit -m "With theme minimal mistake"
+git commit -m $message
 git push
