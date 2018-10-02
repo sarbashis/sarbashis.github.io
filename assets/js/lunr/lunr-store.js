8a2b7cd11b492c3f02d93c2c93779029d2de6b56
Sarbashis Das <sarbashis@users.noreply.github.com>
2018-10-02 10:28:51 +0200

    New Post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,16 +1,16 @@
 var store = [{
         "title": "How to Set Personal WebPage in Github",
         "excerpt":"This posts is going to show how to setup github page using jekylland minima-mistakes theme ","categories": ["github"],
-        "tags": ["comments","layout"],
+        "tags": ["comments"],
         "url": "http://localhost:4000/github/2018/how-to-setting-personal-website-in-github/",
         "teaser":null},{
         "title": "How to Setup Parallel in Mac",
         "excerpt":"Parallel is a perl script that allow user to run same job parallel easily. Installation: &gt; brew install parallel  May be you need to change the permission of the    installed directory&gt;  sudo chown -R $(whoami) /usr/local/sbin","categories": ["github"],
-        "tags": ["layout"],
+        "tags": [],
         "url": "http://localhost:4000/github/2018/how-to-setup-parallel-in-mac/",
         "teaser":null},{
         "title": "Deep Learning Research Articles",
-        "excerpt":"  Wainberg, M., Merico, D., Delong, A., &amp; Frey, B. J. (2018). Deep learning in biomedicine. Nature Biotechnology, 36(9), 829–838.(https://www.nature.com/articles/nbt.4233)","categories": ["DeepLearning"],
+        "excerpt":"  Wainberg, M., Merico, D., Delong, A., &amp; Frey, B. J. (2018).   Deep learning in biomedicine. Nature Biotechnology, 36(9), 829–838.","categories": ["DeepLearning"],
         "tags": ["ML"],
         "url": "http://localhost:4000/deeplearning/2018/Deep-Learning-research-articles/",
         "teaser":null}]
