9e5380758c5de7bcb70c29aa8f400d8c3307324a
Sarbashis Das <sarbashis@users.noreply.github.com>
2018-10-03 10:28:43 +0200

    New Post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,9 +5,9 @@ var store = [{
         "url": "http://localhost:4000/github/2018/how-to-setting-personal-website-in-github/",
         "teaser":null},{
         "title": "How to Setup Parallel in Mac",
-        "excerpt":"Parallel is a perl script that allow user to run same job parallel easily. Installation: &gt; brew install parallel  May be you need to change the permission of the    installed directory&gt;  sudo chown -R $(whoami) /usr/local/sbin","categories": ["github"],
+        "excerpt":"Parallel is a perl script that allow user to run same job parallel easily. Installation: &gt; brew install parallel  May be you need to change the permission of the    installed directory&gt;  sudo chown -R $(whoami) /usr/local/sbin","categories": ["mac"],
         "tags": [],
-        "url": "http://localhost:4000/github/2018/how-to-setup-parallel-in-mac/",
+        "url": "http://localhost:4000/mac/2018/how-to-setup-parallel-in-mac/",
         "teaser":null},{
         "title": "Deep Learning Research Articles",
         "excerpt":"  Wainberg, M., Merico, D., Delong, A., &amp; Frey, B. J. (2018).   Deep learning in biomedicine. Nature Biotechnology, 36(9), 829–838.","categories": ["DeepLearning"],
